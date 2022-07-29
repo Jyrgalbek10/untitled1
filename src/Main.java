47ff3cea1188436a131d31837dc11795d09e97e2
Jyrgalbek10 <jyrgaltagaev170817@gmail.com>
2022-07-29 22:18:12 +0300

    Birinchi kament

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+        public class Main {
+            public static void main(String[] args) {
+
+
+                Random ruk = new Random();
+                double rok = ruk.nextDouble();
+                System.out.println(rok);
+            }
+            
+    }
+    
+    
+
+
+
+// int a = 18;
+// int b = 10;
+// int c = 20;
+//
+//  System.out.println(  (a + b + c) / 3 );
+//
+//    }
+//    }
+//        Scanner bum = new Scanner(System.in);
+//      float bir = 39;//.nextFloat();
+//       System.out.println("Birinchi: %d %s"+bir+"misal");
+//    float eki = 45;
+//        System.out.println("Ekinchi: %d %d"+eki+"misal");
+//      float uch = bum.nextFloat();
+//        System.out.println("Birinchi: %d %f "+uch+"misal");
+//        float tort = bum.nextFloat();
+//        System.out.println("Birinchi: %d %n"+tort+"misal");
+//
+//    }
+//}
+
+public class Main {
+    Scanner well = new Scanner(System.in);
+}
